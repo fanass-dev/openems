944ebb545b3d886856f34709d705ac683e098cbe
Hans Krämer <Hans.Kraemer@posteo.de>
2024-02-13 09:22:21 +0100

    Implement Meter Eastron SDM 120

--- io.openems.edge.meter.api/src/io/openems/edge/meter/api/SinglePhaseMeter.java
+++ io.openems.edge.meter.api/src/io/openems/edge/meter/api/SinglePhaseMeter.java
@@ -77,6 +77,49 @@ public interface SinglePhaseMeter extends ElectricityMeter {
 	}
 
 	/**
+	 * Initializes Channel listeners for a {@link SinglePhaseMeter}.
+	 * 
+	 * <p>
+	 * Sets the correct value for {@link ChannelId#ACTIVE_POWER_L1},
+	 * {@link ChannelId#ACTIVE_POWER_L2} or {@link ChannelId#ACTIVE_POWER_L3} from
+	 * {@link ChannelId#ACTIVE_POWER} by evaluating the configured
+	 * {@link SinglePhase} via {@link SinglePhaseMeter#getPhase()}.
+	 *
+	 * @param meter the {@link SinglePhaseMeter}
+	 */
+	public static void calculateSinglePhaseFromVoltage(SinglePhaseMeter meter) {
+		SinglePhaseMeter.calculateSinglePhaseFromVoltage(meter, SinglePhaseMeter::getPhase);
+	}
+
+	/**
+	 * Initializes Channel listeners for a {@link SinglePhaseMeter}.
+	 * 
+	 * <p>
+	 * Use this method if it is not known at compile time, that the
+	 * {@link ElectricityMeter} is a {@link SinglePhaseMeter}, i.e. it is not
+	 * implementing {@link SinglePhaseMeter}.
+	 * 
+	 * <p>
+	 * Sets the correct value for {@link ChannelId#VOLTAGE_L1},
+	 * {@link ChannelId#VOLtAGe_L2} or {@link ChannelId#VOLTAGE_L3} from
+	 * {@link ChannelId#VOLTAGE} by evaluating the provided
+	 * {@link SinglePhase}.
+	 *
+	 * @param <METER>       type that extends {@link ElectricityMeter}
+	 * @param meter         a {@link ElectricityMeter}
+	 * @param phaseProvider a provider for {@link SinglePhase}
+	 */
+	public static <METER extends ElectricityMeter> void calculateSinglePhaseFromVoltage(METER meter,
+			Function<METER, SinglePhase> phaseProvider) {
+		meter.getVoltageChannel().onSetNextValue(value -> {
+			var phase = phaseProvider.apply(meter);
+			meter.getVoltageL1Channel().setNextValue(phase == SinglePhase.L1 ? value : null);
+			meter.getVoltageL2Channel().setNextValue(phase == SinglePhase.L2 ? value : null);
+			meter.getVoltageL3Channel().setNextValue(phase == SinglePhase.L3 ? value : null);
+		});
+	}
+
+	/**
 	 * Used for Modbus/TCP Api Controller. Provides a Modbus table for the Channels
 	 * of this Component.
 	 *
